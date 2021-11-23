a4d362b808ea9858887ba4a0d6bc9472e4e9b772
alexandraalchin <alchinalexandra@gmail.com>
2021-11-22 20:20:21 -0700

    Created own css themes
    Ocean, Seaweed, Otter colorway

--- 19.2-activities/02_theme_changer/unsolved/js/themeChange.js
+++ 19.2-activities/02_theme_changer/unsolved/js/themeChange.js
@@ -1,11 +1,11 @@
 $(".themeChange").on("click", function() {
     if ($(this).hasClass("regular")) {
-        $("#theme").attr("href", "css/blank.css")
+        $("#theme").attr("href", "css/ocean.css")
     }
     else if ($(this).hasClass("blue")) {
-        $("#theme").attr("href", "css/blue.css")
+        $("#theme").attr("href", "css/seaweed.css")
     }
     else if ($(this).hasClass("dark")) {
-        $("#theme").attr("href", "css/dark.css")
+        $("#theme").attr("href", "css/otter.css")
     }
 });
