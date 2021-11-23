a8c61722c5dc66dab4e3c981d4dc938efae040b9
alexandraalchin <alchinalexandra@gmail.com>
2021-11-22 20:10:05 -0700

    Activity 3 Started
    I started it

--- 19.2-activities/03_todo_app/unsolved/js/index.js
+++ 19.2-activities/03_todo_app/unsolved/js/index.js
@@ -1,5 +1,9 @@
 // Create event listener to enter items below
+$("#enter").on("click", function () {
+    var task = $("todoItem").val();
+    $("#todoList").append("<div class='task'>" + task + "<div class='x fas fa-times'>")
 
+});
 // Create event listener to delete items below
 
 // Create event listener to toggle 
