373f4efe8dc38ff19d601a18e848950033bc4385
alexandraalchin <alchinalexandra@gmail.com>
2021-11-22 18:32:47 -0700

    I messed up - Activity 19.2 1
    Ac

--- 19.2-activities/01_Conditionals/unsolved/js/index.js
+++ 19.2-activities/01_Conditionals/unsolved/js/index.js
@@ -3,12 +3,12 @@ $('.tab').on("click", function(){
     $(".tab1").addClass("active");
     $(".tab2").removeClass("active");
     $("#tabContent1").css("display", "block");
-    $("#tabContent2").css("display", "block");
+    $("#tabContent2").css("display", "none");
   }
   else if ($(this).hasClass("tab2")) {
-    $(".tab1").addClass("active");
-    $(".tab2").removeClass("active");
-    $("#tabContent1").css("display", "block");
+    $(".tab2").addClass("active");
+    $(".tab1").removeClass("active");
     $("#tabContent2").css("display", "block");
+    $("#tabContent1").css("display", "none");
   }
 });
